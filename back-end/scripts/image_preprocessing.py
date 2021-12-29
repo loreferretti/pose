daf49c661c1cb655df88bced3619e74918f48086
Maria Giovanna Donadio <maria.donadio@stud.unifi.it>
2021-12-29 16:32:29 +0100

    aggiunta funzione distanza

--- back-end/scripts/image_preprocessing.py
+++ back-end/scripts/image_preprocessing.py
@@ -1,3 +1,4 @@
+from os import name
 import tensorflow as tf
 import numpy as np
 import tensorflow_hub as hub
@@ -6,9 +7,11 @@ from matplotlib import pyplot as plt
 from matplotlib.collections import LineCollection
 import matplotlib.patches as patches
 import cv2
+import json
 
 module = hub.load("https://tfhub.dev/google/movenet/singlepose/thunder/4")
 input_size = 256
+
 
 def movenet(input_image):
     """Runs detection on an input image.
@@ -29,121 +32,13 @@ def movenet(input_image):
     # Run model inference.
     outputs = model(input_image)
     # Output is a [1, 1, 17, 3] tensor.
+    print(outputs)
     keypoints_with_scores = outputs['output_0'].numpy()
     return keypoints_with_scores
 
 
-
-# map bones to matplotlib color name
-KEYPOINT_EDGE_INDS_TO_COLOR = {
-     (0,1): 'm',
-     (0,2): 'c',
-     (1,3): 'm',
-     (2,4): 'c',
-     (0,5): 'm',
-     (0,6): 'c',
-     (5,7): 'm',
-     (7,9): 'm',
-     (6,8): 'c',
-     (8,10): 'c',
-     (5,6): 'y',
-     (5,11): 'm',
-     (6,12): 'c',
-     (11,12): 'y',
-     (11,13): 'm',
-     (13,15): 'm',
-     (12,14): 'c',
-     (14,16): 'c'
- } 
-
-def _keypoints_and_edges_for_display(keypoints_with_score,height,
-                                      width,keypoint_threshold=0.11):
-   """Returns high confidence keypoints and edges"""
-   keypoints_all = []
-   keypoint_edges_all = []
-   edge_colors = []
-   num_instances,_,_,_ = keypoints_with_score.shape
-   for id in range(num_instances):
-     kpts_x = keypoints_with_score[0,id,:,1]
-     kpts_y = keypoints_with_score[0,id,:,0]
-     kpts_scores = keypoints_with_score[0,id,:,2]
-     kpts_abs_xy = np.stack(
-         [width*np.array(kpts_x),height*np.array(kpts_y)],axis=-1)
-     kpts_above_thrs_abs = kpts_abs_xy[kpts_scores > keypoint_threshold,: ]
-     keypoints_all.append(kpts_above_thrs_abs)
-     for edge_pair,color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
-       if (kpts_scores[edge_pair[0]] > keypoint_threshold and 
-           kpts_scores[edge_pair[1]] > keypoint_threshold):
-         x_start = kpts_abs_xy[edge_pair[0],0]
-         y_start = kpts_abs_xy[edge_pair[0],1]
-         x_end = kpts_abs_xy[edge_pair[1],0]
-         y_end = kpts_abs_xy[edge_pair[1],1]
-         lien_seg = np.array([[x_start,y_start],[x_end,y_end]])
-         keypoint_edges_all.append(lien_seg)
-         edge_colors.append(color)
-   if keypoints_all:
-     keypoints_xy = np.concatenate(keypoints_all,axis=0)
-   else:
-     keypoints_xy = np.zeros((0,17,2))
-   if keypoint_edges_all:
-     edges_xy = np.stack(keypoint_edges_all,axis=0)
-   else:
-     edges_xy = np.zeros((0,2,2))
-   return keypoints_xy,edges_xy,edge_colors     
-
-def draw_prediction_on_image(
-     image, keypoints_with_scores, crop_region=None, close_figure=False,
-     output_image_height=None):
-   """Draws the keypoint predictions on image"""
-   height, width, channel = image.shape
-   aspect_ratio = float(width) / height
-   fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
-   # To remove the huge white borders
-   fig.tight_layout(pad=0)
-   ax.margins(0)
-   ax.set_yticklabels([])
-   ax.set_xticklabels([])
-   plt.axis('off')
-   im = ax.imshow(image)
-   line_segments = LineCollection([], linewidths=(4), linestyle='solid')
-   ax.add_collection(line_segments)
-   # Turn off tick labels
-   scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
-   (keypoint_locs, keypoint_edges,
-    edge_colors) = _keypoints_and_edges_for_display(
-        keypoints_with_scores, height, width)
-   line_segments.set_segments(keypoint_edges)
-   line_segments.set_color(edge_colors)
-   if keypoint_edges.shape[0]:
-     line_segments.set_segments(keypoint_edges)
-     line_segments.set_color(edge_colors)
-   if keypoint_locs.shape[0]:
-     scat.set_offsets(keypoint_locs)
-   if crop_region is not None:
-     xmin = max(crop_region['x_min'] * width, 0.0)
-     ymin = max(crop_region['y_min'] * height, 0.0)
-     rec_width = min(crop_region['x_max'], 0.99) * width - xmin
-     rec_height = min(crop_region['y_max'], 0.99) * height - ymin
-     rect = patches.Rectangle(
-         (xmin,ymin),rec_width,rec_height,
-         linewidth=1,edgecolor='b',facecolor='none')
-     ax.add_patch(rect)
-   fig.canvas.draw()
-   image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-   """ image_from_plot = image_from_plot.reshape(
-       fig.canvas.get_width_height()[::-1] + (3,)) """
-   image_from_plot = image_from_plot.reshape(
-       2400, 2400, 3)
-   plt.close(fig)
-   if output_image_height is not None:
-     output_image_width = int(output_image_height / height * width)
-     image_from_plot = cv2.resize(
-         image_from_plot, dsize=(output_image_width, output_image_height),
-          interpolation=cv2.INTER_CUBIC)
-   return image_from_plot     
-
 # Load the input image.
-image_path = '../assets/7.jpeg'
+image_path = '../assets/16.jpg'
 image = tf.io.read_file(image_path)
 image = tf.image.decode_jpeg(image)
 
@@ -152,15 +47,24 @@ input_image = tf.expand_dims(image, axis=0)
 input_image = tf.image.resize_with_pad(input_image, input_size, input_size)
 
 # Run model inference.
-keypoints_with_scores = movenet(input_image) 
+keypoints = movenet(input_image)
 
-# Visualize the predictions with image.
-display_image = tf.expand_dims(image, axis=0)
-display_image = tf.cast(tf.image.resize_with_pad(display_image, 1280, 1280), dtype=tf.int32)
-output_overlay = draw_prediction_on_image(np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+# ndarray in json
+keypoints_list = keypoints.tolist()
 
-plt.figure(figsize=(5, 5))
-plt.imshow(output_overlay) 
-_ = plt.axis('off')
+# index: {y:   , x:   , score:   , name:   }
+part_list = ["nose", "left_eye", "right_eye", "left_ear", "right_ear", "left_shoulder", "right_shoulder", "left_elbow",
+             "right_elbow", "left_wrist", "right_wrist", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle"]
 
-plt.show()
+dictionary = {
+    "score": keypoints_list[0],
+    "keypoints": [{"y": y, "x": x, "score": score, "name": part_list[idx]} for idx, (y, x, score) in enumerate(keypoints_list[0][0])],
+    "path": image_path,
+}
+
+# Serializing json
+json_object = json.dumps(dictionary, indent=4)
+
+# Writing to sample.json
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
--- front-end/scripts/initialize.js
+++ front-end/scripts/initialize.js
@@ -5,115 +5,252 @@
 // Again the lower the number, the closer the distance
 
 function weightedDistanceMatching(poseA, poseB) {
-  /* let vector1PoseXY = poseVector1.slice(0, 34);
-  let vector1Confidences = poseVector1.slice(34, 51);
-  let vector1ConfidenceSum = poseVector1.slice(51, 52);
-
-  let vector2PoseXY = poseVector2.slice(0, 34);
- */
-  // First summation
-  //let summation1 = 1 / vector1ConfidenceSum;
-
-  // Second summation
-  /* let summation2 = 0;
-  for (let i = 0; i < vector1PoseXY.length; i++) {
-    let tempConf = Math.floor(i / 2);
-    let tempSum = vector1Confidences[tempConf] * Math.abs(vector1PoseXY[i] - vector2PoseXY[i]);
-    summation2 = summation2 + tempSum;
-  } */
-  const summation1 = 1 / poseA.score;
-  const summation2 = poseA.keypoints.reduce((res, val, idx) => res + val.score * (Math.abs(val.x - poseB.keypoints[idx].x) + Math.abs(val.y - poseB.keypoints[idx].y)), 0);
-
-  return summation1 * summation2;
+  const count = poseA.keypoints.reduce((res, kpA) => {
+    if (poseB.keypoints.find((kpB) => kpA.name === kpB.name)) {
+      return res + 1;
+    }
+    return res;
+  }, 0);
+  if (count < poseA.keypoints.length / 2) {
+    return Infinity;
+  }
+  return poseA.keypoints.reduce((res, kpA) => {
+    const kpB = poseB.keypoints.find((kpB) => kpA.name === kpB.name);
+    if (!kpB) {
+      return res + kpA.score * 2; //2 è la distanza massima che si può raggiungere
+    }
+    return res + kpA.score * (Math.abs(kpA.x - kpB.x) + Math.abs(kpA.y - kpB.y));
+  }, 0);
 }
 
-const setup = async () => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-  const VIDEO_WIDTH = 640;
-  const VIDEO_HEIGHT = 480;
-  const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, {
-    modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTING,
-  });
-  const video = $("#video").get(0);
-  function drawPoint(y, x, r) {
+const VIDEO_WIDTH = 512;
+const VIDEO_HEIGHT = 512;
+const FRAME_RATE = 20;
+const USE_IMAGE = false;
+
+const drawPoint = ({ ctx, x, y, r }) => {
+  ctx.beginPath();
+  ctx.arc(x * VIDEO_WIDTH, y * VIDEO_HEIGHT, r, 0, 2 * Math.PI);
+  ctx.fillStyle = "#000000";
+  ctx.fill();
+};
+const drawSegment = ({ ctx, pointA, pointB, color, scale }) => {
+  if (pointA && pointB) {
     ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.fillStyle = "#000000";
-    ctx.fill();
-  }
-  function drawKeypoints(keypoints) {
-    for (let i = 0; i < keypoints.length; i++) {
-      const keypoint = keypoints[i];
-      console.log(`keypoint in drawkeypoints ${keypoint}`);
-      const { y, x } = keypoint;
-      drawPoint(y, x, 6);
-    }
-  }
-  function drawSegment(pair1, pair2, color, scale) {
-    ctx.beginPath();
-    ctx.moveTo(pair1.x * scale, pair1.y * scale);
-    ctx.lineTo(pair2.x * scale, pair2.y * scale);
+    ctx.moveTo(pointA.x * scale * VIDEO_WIDTH, pointA.y * scale * VIDEO_WIDTH);
+    ctx.lineTo(pointB.x * scale * VIDEO_WIDTH, pointB.y * scale * VIDEO_HEIGHT);
     ctx.lineWidth = 2;
     ctx.strokeStyle = color;
     ctx.stroke();
   }
-
-  function drawSkeleton(keypoints) {
-    const color = "#8f00ff";
-    const adjacentKeyPoints = [
-      ["nose", "left_eye"],
-      ["nose", "right_eye"],
-      ["left_eye", "left_ear"],
-      ["right_eye", "right_ear"],
-      ["left_shoulder", "right_shoulder"],
-      ["left_shoulder", "left_elbow"],
-      ["left_elbow", "left_wrist"],
-      ["right_shoulder", "right_elbow"],
-      ["right_elbow", "right_wrist"],
-      ["left_shoulder", "left_hip"],
-      ["right_shoulder", "right_hip"],
-      ["left_hip", "right_hip"],
-      ["left_hip", "left_knee"],
-      ["left_knee", "left_ankle"],
-      ["right_hip", "right_knee"],
-      ["right_knee", "right_ankle"],
-    ];
-
-    adjacentKeyPoints.forEach(([first, second]) => {
-      drawSegment(
-        keypoints.find(({ name }) => name === first),
-        keypoints.find(({ name }) => name === second),
-        color,
-        1
-      );
-    });
-  }
-
-  const webcam = new Webcam(video, "user");
-  webcam
-    .start()
-    .then((result) => {
-      console.log("webcam started");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  setInterval(async () => {
-    const poses = await detector.estimatePoses(video);
-    console.log("poses", poses);
-    canvas.width = VIDEO_WIDTH;
-    canvas.height = VIDEO_HEIGHT;
-    ctx.clearRect(0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
-    ctx.save();
-    ctx.drawImage(video, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
-    ctx.restore();
-    poses.forEach(({ score, keypoints }) => {
-      const filteredKeypoints = keypoints.filter(({ score }) => score > 0.3);
-      drawKeypoints(filteredKeypoints);
-      drawSkeleton(filteredKeypoints);
-    });
-  }, 1000 / 20);
 };
 
-$(setup);
+const drawKeypoints = ({ ctx, keypoints }) => {
+  keypoints.forEach(({ x, y }) => {
+    drawPoint({ ctx, x, y, r: 6 });
+  });
+};
+const drawSkeleton = ({ ctx, keypoints }) => {
+  const color = "#8f00ff";
+  const adjacentKeyPoints = [
+    ["nose", "left_eye"],
+    ["nose", "right_eye"],
+    ["left_eye", "left_ear"],
+    ["right_eye", "right_ear"],
+    ["left_shoulder", "right_shoulder"],
+    ["left_shoulder", "left_elbow"],
+    ["left_elbow", "left_wrist"],
+    ["right_shoulder", "right_elbow"],
+    ["right_elbow", "right_wrist"],
+    ["left_shoulder", "left_hip"],
+    ["right_shoulder", "right_hip"],
+    ["left_hip", "right_hip"],
+    ["left_hip", "left_knee"],
+    ["left_knee", "left_ankle"],
+    ["right_hip", "right_knee"],
+    ["right_knee", "right_ankle"],
+  ];
+  adjacentKeyPoints.forEach(([first, second]) => {
+    drawSegment({
+      ctx,
+      pointA: keypoints.find(({ name }) => name === first),
+      pointB: keypoints.find(({ name }) => name === second),
+      color,
+      scale: 1,
+    });
+  });
+};
+
+const addImageProcess = (src) =>
+  new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = src;
+  });
+
+$(async () => {
+  const video = $("#video").get(0);
+  video.width = VIDEO_WIDTH;
+  video.height = VIDEO_HEIGHT;
+  const canvas = $("#canvas").get(0);
+  canvas.width = VIDEO_WIDTH;
+  canvas.height = VIDEO_HEIGHT;
+  const webcam = new Webcam(video, "user");
+  const ctx = canvas.getContext("2d");
+  const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, {
+    modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTING,
+  });
+
+  const img = await addImageProcess("../back-end/assets/16.jpg");
+  await webcam.stream();
+
+  const imagePoses = [
+    {
+      score: 0.7,
+      keypoints: [
+        {
+          y: 0.3062269389629364,
+          x: 0.504449188709259,
+          score: 0.7027429342269897,
+          name: "nose",
+        },
+        {
+          y: 0.2922380566596985,
+          x: 0.5168091058731079,
+          score: 0.7404518127441406,
+          name: "left_eye",
+        },
+        {
+          y: 0.29420435428619385,
+          x: 0.4906785786151886,
+          score: 0.72865891456604,
+          name: "right_eye",
+        },
+        {
+          y: 0.2957649528980255,
+          x: 0.5302802920341492,
+          score: 0.6799468994140625,
+          name: "left_ear",
+        },
+        {
+          y: 0.3016800880432129,
+          x: 0.47342240810394287,
+          score: 0.7859954833984375,
+          name: "right_ear",
+        },
+        {
+          y: 0.34616392850875854,
+          x: 0.5627409219741821,
+          score: 0.8367493152618408,
+          name: "left_shoulder",
+        },
+        {
+          y: 0.3500824570655823,
+          x: 0.4436020255088806,
+          score: 0.8595637083053589,
+          name: "right_shoulder",
+        },
+        {
+          y: 0.2704964876174927,
+          x: 0.619113564491272,
+          score: 0.932282567024231,
+          name: "left_elbow",
+        },
+        {
+          y: 0.2686147093772888,
+          x: 0.380423367023468,
+          score: 0.8752314448356628,
+          name: "right_elbow",
+        },
+        {
+          y: 0.18335765600204468,
+          x: 0.582643449306488,
+          score: 0.8813289999961853,
+          name: "left_wrist",
+        },
+        {
+          y: 0.18383997678756714,
+          x: 0.4065172076225281,
+          score: 0.8114475607872009,
+          name: "right_wrist",
+        },
+        {
+          y: 0.550326943397522,
+          x: 0.5428749322891235,
+          score: 0.7955687642097473,
+          name: "left_hip",
+        },
+        {
+          y: 0.5508424639701843,
+          x: 0.46621716022491455,
+          score: 0.8097958564758301,
+          name: "right_hip",
+        },
+        {
+          y: 0.6831645965576172,
+          x: 0.6225772500038147,
+          score: 0.923366904258728,
+          name: "left_knee",
+        },
+        {
+          y: 0.6897925734519958,
+          x: 0.3979915678501129,
+          score: 0.8628276586532593,
+          name: "right_knee",
+        },
+        {
+          y: 0.8053678274154663,
+          x: 0.6417670845985413,
+          score: 0.8776750564575195,
+          name: "left_ankle",
+        },
+        {
+          y: 0.8178161382675171,
+          x: 0.36935141682624817,
+          score: 0.9401204586029053,
+          name: "right_ankle",
+        },
+      ],
+    },
+  ];
+
+  const normImagePoses = imagePoses.map(({ score, keypoints }) => ({
+    score,
+    keypoints: keypoints.map(({ x, y, score, name }) => ({
+      x: (x * img.width) / img.width,
+      y: (y * img.width - Math.abs(img.width - img.height) / 2) / img.height,
+      score,
+      name,
+    })),
+  }));
+
+  let distance = Infinity;
+
+  setInterval(async () => {
+    const videoPoses = USE_IMAGE ? [] : await detector.estimatePoses(video);
+    const normVideoPoses = videoPoses.map(({ score, keypoints }) => ({
+      score,
+      keypoints: keypoints
+        .filter((kp) => kp.score > 0.3)
+        .map(({ x, y, score, name }) => ({
+          x: x / 620,
+          y: y / 480,
+          score,
+          name,
+        })),
+    }));
+    const poses = USE_IMAGE ? normImagePoses : normVideoPoses;
+    /* console.log("POSES", poses); */
+    distance = Math.min(distance, weightedDistanceMatching(normImagePoses[0], normVideoPoses[0]));
+    $("#score").text(distance);
+    ctx.clearRect(0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
+    ctx.save();
+    ctx.drawImage(USE_IMAGE ? img : video, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
+    ctx.restore();
+    poses.forEach(({ keypoints }) => {
+      drawKeypoints({ ctx, keypoints });
+      drawSkeleton({ ctx, keypoints });
+    });
+  }, 1000 / FRAME_RATE);
+});
