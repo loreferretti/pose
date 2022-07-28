a7ecdd76a65abdb7b6a6f9c8e935e2ee22daaeb7
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-07-28 17:49:11 +0200

    change http to https protocol

--- front-end/scripts/config.js
+++ front-end/scripts/config.js
@@ -1,5 +1,5 @@
 export class Config {
-  static SERVER_URL = "http://strikeapose.it/";
+  static SERVER_URL = "https://strikeapose.it/";
   static BASE_URL = `${this.SERVER_URL}api/v1/`;
   static WIDTH = 1024;
   static HEIGHT = 1024;
