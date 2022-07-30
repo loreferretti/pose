70fad5b2fac6a488796f5f7fd11ba07599b70cea
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-07-30 17:17:46 +0200

    add regex for email validation and set response body message to paragraph

--- front-end/index.js
+++ front-end/index.js
@@ -1,7 +1,29 @@
 import { Config } from "./scripts/config.js";
 
+function isValid(email) {
+  const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const regex = new RegExp(pattern);
+  return regex.test(email)
+}
+
 $(() => {
+
   const form = $("#login-form");
+  const email = $("input[name='email']");
+  const submit = $(":submit");
+
+  submit.prop("disabled", true);
+
+  email.on("input", function(){
+    if(isValid($(this).val())) {
+      $(":submit").prop("disabled", false);
+    }
+  });
+
+  form.on("input", function() {
+    $("#response-message").empty();
+  });
+
   form.submit(async (e) => {
     e.preventDefault();
     const data = form.serializeArray().reduce(
@@ -23,6 +45,8 @@ $(() => {
     if (response.ok) {
       localStorage.setItem("ACCESS_TOKEN", jsonResponse.access_token);
       location.href = "start.html";
+    } else {
+      $("#response-message").text(jsonResponse);
     }
     return false;
   });
