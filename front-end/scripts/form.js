12503f8909e3739dd2a533e6d902d06d26d76aeb
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-08-11 18:14:24 +0200

    add file for checking input params in the login and signup page

---
+++ front-end/scripts/form.js
@@ -0,0 +1,34 @@
+import {Config} from "./config.js";
+
+function isValid(email) {
+  const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const regex = new RegExp(pattern);
+  return regex.test(email)
+}
+
+export function checkEmptyInputs(email, password) {
+  return !email || !password;
+}
+
+export function checkValidity(email, password) {
+  return !checkEmptyInputs(email, password) && isValid(email);
+}
+
+export async function send(form, endpoint) {
+  const data = form.serializeArray().reduce(
+    (res, val) => ({
+      ...res,
+      [val.name]: val.value,
+    }),
+    {}
+  );
+  
+  const response = await fetch(`${Config.BASE_URL}${endpoint}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response;
+}
