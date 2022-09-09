df8555ee839f8a24000ee81fe89e0be9a71e8d2f
loreferretti <lorenzo.ferretti3@stud.unifi.it>
2022-09-09 03:13:30 +0200

    add development config and production config

--- back-end/app.py
+++ back-end/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="https://strikeapose.it")
 cors = CORS(app, resources={r"/*": {"origins": "https://strikeapose.it"}})
 
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object("config.DevelopmentConfig")
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -17,4 +17,9 @@ bcrypt = Bcrypt(app)
 from views import *
 
 if __name__ == "__main__":
-    app.run()
+    #DEVELOPMENT CONFIG
+	app.run(host="0.0.0.0")
+
+    #PRODUCTION CONFIG
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=5000)
